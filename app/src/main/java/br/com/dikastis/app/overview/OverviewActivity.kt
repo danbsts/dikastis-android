9882c0adebbefed8022e19e7ddbfaaa6659bc8a5
Rodrigo Rodrigues <32945462+rodz@users.noreply.github.com>
2021-04-30 23:10:24 -0300

    Feat/add chart (#1)
    * wip
    
    * Add new chart button to task activity and create overview activity
    
    * Update constants
    
    * Add new chart screen and shortcut
    
    * Change config

---
+++ app/src/main/java/br/com/dikastis/app/overview/OverviewActivity.kt
@@ -0,0 +1,70 @@
+package br.com.dikastis.app.overview
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.dikastis.app.databinding.ActivityOverviewBinding
+import br.com.dikastis.app.model.Constants
+import br.com.dikastis.app.model.Problem
+import br.com.dikastis.app.model.Student
+import br.com.dikastis.app.model.Submission
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
+
+class OverviewActivity : AppCompatActivity(){
+    private lateinit var binding : ActivityOverviewBinding
+
+    val aaChartModel : AAChartModel = AAChartModel()
+        .chartType(AAChartType.Bar)
+        .title("Overview da tarefa")
+        .subtitle("Numero de aceitos por aluno")
+        .dataLabelsEnabled(true)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityOverviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val chartView = binding.aaChartView
+        chartView.apply {
+            setNameAndValue(Constants.students, Constants.problems)
+            aa_drawChartWithChartModel(aaChartModel)
+        }
+    }
+
+    fun setNameAndValue(students: Array<Student>, problems: Array<Problem>) {
+        var nomes : Array<String> = problems.map { problem -> problem.name }.toTypedArray()
+        var accepted : Array<Int> = Array(nomes.size) { 0 }
+        var tried : Array<Int> = Array(accepted.size) { 0 }
+
+        for (student : Student in students) {
+            for(i in nomes.indices) {
+                var submited = 0
+                var done = 0
+                println(nomes[i])
+                for(submission: Submission in student.submissions) {
+                    if(submission.problemId == nomes[i]) {
+                        submited = 1
+                        if(submission.status == "ACCEPTED") {
+                            done = 1
+                        }
+                    }
+                }
+                accepted[i] = accepted[i].plus(done)
+                tried[i] = tried[i].plus(submited)
+            }
+        }
+
+        var acceptedResult : Array<Any>  = accepted.map { i -> i }.toTypedArray()
+        var triedResult : Array<Any>  = tried.map { i -> i }.toTypedArray()
+
+        aaChartModel
+            .categories(nomes.clone())
+            .series(
+                arrayOf(
+                    AASeriesElement().name("Accepted").data(acceptedResult),
+                    AASeriesElement().name("Tried").data(triedResult))
+            )
+    }
+}
--- app/src/main/java/br/com/dikastis/app/task/TaskActivity.kt
+++ app/src/main/java/br/com/dikastis/app/task/TaskActivity.kt
@@ -1,12 +1,13 @@
 package br.com.dikastis.app.task
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.com.dikastis.app.databinding.ActivityTaskBinding
-import br.com.dikastis.app.databinding.ActivityTeamBinding
 import br.com.dikastis.app.model.Constants
+import br.com.dikastis.app.overview.OverviewActivity
 import br.com.dikastis.app.overview.OverviewAdapter
 
 class TaskActivity : AppCompatActivity(){
@@ -16,9 +17,9 @@ class TaskActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val recyclerViewTeams = binding.taskList
-        val name = intent.getStringExtra("name")
-        recyclerViewTeams.apply {
+        val recyclerViewStudents = binding.studentsList
+        val button = binding.buttonChart
+        recyclerViewStudents.apply {
             layoutManager = LinearLayoutManager(this@TaskActivity)
             addItemDecoration(DividerItemDecoration(this@TaskActivity, DividerItemDecoration.VERTICAL))
             adapter = OverviewAdapter(
@@ -26,6 +27,12 @@ class TaskActivity : AppCompatActivity(){
                     Constants.problems,
                     layoutInflater
             )
+        }
+
+        button.setOnClickListener {
+            val c = binding.buttonChart.context
+            val intentExplicito = Intent(c, OverviewActivity::class.java)
+            c.startActivity(intentExplicito)
         }
 
     }
